be1edcacb42ae90adac27cf11225ac72cfbda3ee
Mariusz Woloszyn <emsi@users.noreply.github.com>
2016-05-31 15:58:43 +0200

    comments and formating

--- Get Ubuntu Cloud Image.ps1
+++ Get Ubuntu Cloud Image.ps1
@@ -15,49 +15,50 @@ $vhdxPath = "\\$vmmLibraryPath\path\to\vhd\images\"
 
 function getUbuntuImage ()
 {
-Param($ubuntuVersion, $ubuntuTag, $ubuntuOperatingSystem, $ubuntuFamilyName,$force=0,$dailyImage=0)
-
-$vhdxFile = "$($vhdxPath)\$ubuntuVersion.vhdx"
-$vhdFile="$($vhdPath)\$ubuntuVersion-server-cloudimg-amd64-disk1.vhd"
-
-# construct image and manifest URL
-if ($dailyImage) {
-  $imageUrl = "https://cloud-images.ubuntu.com/$ubuntuVersion/current/$ubuntuVersion-server-cloudimg-amd64-disk1.vhd.zip"
-  $manifestURL = "https://cloud-images.ubuntu.com/$ubuntuVersion/current/$ubuntuVersion-server-cloudimg-amd64.manifest"
-} else {
-  $imageUrl = "https://cloud-images.ubuntu.com/releases/$ubuntuVersion/release/$ubuntuFamilyName-server-cloudimg-amd64-disk1.vhd.zip"
-  $manifestURL = "https://cloud-images.ubuntu.com/releases/$ubuntuVersion/release/$ubuntuFamilyName-server-cloudimg-amd64.manifest"
-}
-
-# Get the timestamp of the latest build on the Ubuntu cloud-images site
-$lastModified=(Invoke-WebRequest $manifestURL).BaseResponse.LastModified
-$stamp = $lastModified.ToFileTimeUtc()
-$release="1."+$lastModified.Date.Year+"."+$lastModified.Date.Month+"."+$lastModified.Date.Day
-
-# image download path
-$imageCacheFile = "$($imageCachePath)\$ubuntuVersion-$($stamp).vhd.zip"
-
-# Check Paths
-if (!(Test-Path $imageCachePath)) {mkdir $imageCachePath}
-
-# Get vhd data from library
-$vhd = Get-SCVirtualHardDisk | Where-Object {$_.Location –eq $vhdxFile} 
-
-# Nothing to do. Most up-to-date image is in library
-if (($vhd.Release -eq $release) -and ($vhd.State -eq "Normal")){
+  Param($ubuntuVersion, $ubuntuTag, $ubuntuOperatingSystem, $ubuntuFamilyName,$force=0,$dailyImage=0)
+  
+  # construct vhd and vhdx file paths
+  $vhdxFile = "$($vhdxPath)\$ubuntuVersion.vhdx"
+  $vhdFile="$($vhdPath)\$ubuntuVersion-server-cloudimg-amd64-disk1.vhd"
+  
+  # construct image and manifest URL
+  if ($dailyImage) {
+    $imageUrl = "https://cloud-images.ubuntu.com/$ubuntuVersion/current/$ubuntuVersion-server-cloudimg-amd64-disk1.vhd.zip"
+    $manifestURL = "https://cloud-images.ubuntu.com/$ubuntuVersion/current/$ubuntuVersion-server-cloudimg-amd64.manifest"
+  } else {
+    $imageUrl = "https://cloud-images.ubuntu.com/releases/$ubuntuVersion/release/$ubuntuFamilyName-server-cloudimg-amd64-disk1.vhd.zip"
+    $manifestURL = "https://cloud-images.ubuntu.com/releases/$ubuntuVersion/release/$ubuntuFamilyName-server-cloudimg-amd64.manifest"
+  }
+  
+  # Get the timestamp of the latest build on the Ubuntu cloud-images site
+  $lastModified=(Invoke-WebRequest $manifestURL).BaseResponse.LastModified
+  $stamp = $lastModified.ToFileTimeUtc()
+  $release="1."+$lastModified.Date.Year+"."+$lastModified.Date.Month+"."+$lastModified.Date.Day
+  
+  # image download path
+  $imageCacheFile = "$($imageCachePath)\$ubuntuVersion-$($stamp).vhd.zip"
+  
+  # Check Paths
+  if (!(Test-Path $imageCachePath)) {mkdir $imageCachePath}
+  
+  # Get vhd data from library
+  $vhd = Get-SCVirtualHardDisk | Where-Object {$_.Location –eq $vhdxFile} 
+  
+  # Nothing to do. Most up-to-date image is in library
+  if (($vhd.Release -eq $release) -and ($vhd.State -eq "Normal")){
     return
-    }
-
-# No up-to-date image file, need to re-download
-if (!(Test-Path $imageCacheFile) -or $force) {
+  }
+  
+  # No up-to-date image file, need to re-download
+  if (!(Test-Path $imageCacheFile) -or $force) {
     "Downloading $imageUrl"
     # If we do not have a matching image - delete the old ones and download the new one
     Remove-Item "$($imageCachePath)\$ubuntuVersion-*.vhd.zip"
     (new-object System.Net.WebClient).DownloadFile("$imageUrl", $imageCacheFile)
-    }
-
-# If there is image file, uncompress it
-if (Test-Path $imageCacheFile) {
+  }
+  
+  # If there is image file, uncompress it
+  if (Test-Path $imageCacheFile) {
     "Uncompressing $imageCacheFile"
     # unzip cannot overwrite, make surwe there is no old target vhd
     cleanupFile($vhdFile)
@@ -65,10 +66,10 @@ if (Test-Path $imageCacheFile) {
     Add-Type -assembly "system.io.compression.filesystem"
     [io.compression.zipfile]::ExtractToDirectory($imageCacheFile, $vhdPath)
     if (Test-Path $vhdFile) {cleanupFile($imageCacheFile)}
-    }
-
-# Is there vhd file for conversion?
-if (Test-Path $vhdFile) {
+  }
+  
+  # Is there vhd file for conversion?
+  if (Test-Path $vhdFile) {
     cleanupFile $vhdxFile
     "Converting to VHDX"
     # Covert to VHDX and resize to 50GB
@@ -77,8 +78,7 @@ if (Test-Path $vhdFile) {
 
     # Set tags
     Set-SCVirtualHardDisk –VirtualHardDisk $vhd -Name $ubuntuVersion -OperatingSystem $ubuntuOperatingSystem  -Tag $ubuntuTag -Release $release -FamilyName $ubuntuFamilyName
-    }
-
+  }
 }
 
 # Refresh library
